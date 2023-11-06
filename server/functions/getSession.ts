1ee7bca61f2b396c18ade02ac705fc562c603ca9
jeffsummer08 <jeff.k.zhou@gmail.com>
2023-11-06 16:47:05 -0600

    steps

--- server/functions/getSession.ts
+++ server/functions/getSession.ts
@@ -1,16 +1,17 @@
 import puppeteer from 'puppeteer';
 
 export default async function getSession(username: string, password: string): Promise<any> {
+    let steps = []
     try {
         const browser = await puppeteer.launch({
             headless: "new"
         })
-        console.log("Browser launched")
+        steps.push("Browser launched")
         const page = await browser.newPage()
-        console.log("Page created")
+        steps.push("Page created")
 
         await page.setRequestInterception(true)
-        console.log("Request interception enabled")
+        steps.push("Request interception enabled")
 
         page.on("request", (request) => {
             if (request.resourceType() === "image" || request.resourceType() === "stylesheet" || request.resourceType() === "font") {
@@ -23,27 +24,27 @@ export default async function getSession(username: string, password: string): Pr
         await page.goto("https://hac.friscoisd.org/HomeAccess/Account/LogOn?ReturnUrl=%2fHomeAccess%2f", {
             waitUntil: "domcontentloaded"
         })
-        console.log("Page loaded")
+        steps.push("Page loaded")
 
         await page.type("#LogOnDetails_UserName", username)
         await page.type("#LogOnDetails_Password", password)
-        console.log("Credentials entered")
+        steps.push("Credentials entered")
         await Promise.all([
             page.waitForNavigation({
                 waitUntil: "domcontentloaded"
             }),
             page.click("#login")
         ])
-        console.log("Logged in")
+        steps.push("Logged in")
 
         const cookies = await page.cookies()
-        console.log("Cookies obtained")
+        steps.push("Cookies obtained")
 
         await browser.close()
-        console.log("Browser closed")
+        steps.push("Browser closed")
 
         return cookies
     } catch {
-        return "error"
+        return steps
     }
 }
