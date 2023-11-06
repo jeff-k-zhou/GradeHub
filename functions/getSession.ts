95954c31de29f4702ff556570228af6c69324b53
jeffsummer08 <jeff.k.zhou@gmail.com>
2023-11-06 14:26:05 -0600

    gradehub alpha

--- functions/getSession.ts
+++ functions/getSession.ts
@@ -1,22 +1,31 @@
 import puppeteer from 'puppeteer';
 
-export default async function getSession(username: string, password: string) {
-    const browser = await puppeteer.launch({
-        headless: "new"
-    })
-    const page = await browser.newPage()
+export default async function getSession(username: string, password: string): Promise<any> {
+    try {
+        const browser = await puppeteer.launch({
+            headless: "new"
+        })
+        const page = await browser.newPage()
 
-    await page.goto("https://hac.friscoisd.org/HomeAccess/Account/LogOn?ReturnUrl=%2fHomeAccess%2f", {
-        waitUntil: "domcontentloaded"
-    })
+        await page.goto("https://hac.friscoisd.org/HomeAccess/Account/LogOn?ReturnUrl=%2fHomeAccess%2f", {
+            waitUntil: "domcontentloaded"
+        })
 
-    await page.type("#LogOnDetails_UserName", username)
-    await page.type("#LogOnDetails_Password", password)
-    await page.click("#login")
+        await page.type("#LogOnDetails_UserName", username)
+        await page.type("#LogOnDetails_Password", password)
+        await Promise.all([
+            page.waitForNavigation({
+                waitUntil: "domcontentloaded"
+            }),
+            page.click("#login")
+        ])
 
-    const cookies = await page.cookies()
+        const cookies = await page.cookies()
 
-    await browser.close()
+        await browser.close()
 
-    return cookies
+        return cookies
+    } catch {
+        return "error"
+    }
 }
