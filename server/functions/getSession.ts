9619e86fb0d7f5f2e3b9ecd551964a93eab154a6
jeffsummer08 <jeff.k.zhou@gmail.com>
2023-11-06 16:38:22 -0600

    opt in

--- server/functions/getSession.ts
+++ server/functions/getSession.ts
@@ -3,7 +3,7 @@ import puppeteer from 'puppeteer';
 export default async function getSession(username: string, password: string): Promise<any> {
     try {
         const browser = await puppeteer.launch({
-            headless: true
+            headless: "new"
         })
         console.log("Browser launched")
         const page = await browser.newPage()
