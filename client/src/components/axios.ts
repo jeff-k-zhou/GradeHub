9ae99409f88e5864a7bba7c084f8e0755fef1a88
jeffsummer08 <jeff.k.zhou@gmail.com>
2023-11-06 20:01:05 -0600

    different baseurl

--- client/src/components/axios.ts
+++ client/src/components/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const client = axios.create({
-    baseURL: "https://gradehubserver.onrender.com/",
+    baseURL: "https://gradehubserver.onrender.com",
 })
 
 export default client
