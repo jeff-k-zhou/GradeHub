85bdee5a92f3a0d0754f60e036e73bf2aabcfaf0
jeffsummer08 <jeff.k.zhou@gmail.com>
2023-11-07 20:12:28 -0600

    gpa hotfix

--- client/src/components/functions/calculateGPA.ts
+++ client/src/components/functions/calculateGPA.ts
@@ -7,11 +7,14 @@ export default function calculateGPA(grades: any[], mp: number) {
     const regex = /([A-Z])\w+/g
     for (const grade of grades) {
         let key = ""
-        Object.keys(classes).forEach((item) => {
-            if (grade.code.includes(item.slice(0, 7))) {
-                key = item
-            }
-        })
+        while (!key) {
+            let i = 7
+            Object.keys(classes).forEach((item) => {
+                if (grade.code.includes(item.slice(0, i))) {
+                    key = item
+                }
+            })
+        }
         console.log(key)
         if (Number(grade.grade) === 0 || isNaN(Number(grade.grade))) {
             if (!isNaN(Number(grade.grade))) {
