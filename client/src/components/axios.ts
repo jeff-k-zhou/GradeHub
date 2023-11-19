0a13b31c4c847428567e12ae3ab59ac6679dadb1
jeffsummer08 <jeff.k.zhou@gmail.com>
2023-11-19 13:44:30 -0600

    new server

--- client/src/components/axios.ts
+++ client/src/components/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const client = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://gradehubfisd-8c535c1aeeab.herokuapp.com/",
 })
 
 export default client
