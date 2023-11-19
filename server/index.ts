3449823c14ae29dbd4f7a888cf85a695fafa3f43
jeffsummer08 <jeff.k.zhou@gmail.com>
2023-11-19 13:50:52 -0600

    hopefully fixed port

--- server/index.ts
+++ server/index.ts
@@ -249,5 +249,5 @@ app.post("/decrypt", (req: Request, res: Response) => {
     })
 })
 
-app.listen(5000, () => console.log("Server running on port 5000"))
+app.listen(process.env.PORT || 5000, () => console.log(`Server running on port ${process.env.PORT || 5000}`))
 
